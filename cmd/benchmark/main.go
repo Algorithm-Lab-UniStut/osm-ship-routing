05142bdcb88c1d8ada2f1925e1b9debcf6cb0155
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2022-08-18 14:12:52 +0200

    use fixed benchmark. read targets from file

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/natevvv/osm-ship-routing/pkg/graph"
@@ -10,6 +14,7 @@ import (
 )
 
 const graphFile = "graphs/ocean_equi_4.fmi"
+const targetFile = "cmd/benchmark/targets.txt"
 
 func main() {
 	start := time.Now()
@@ -20,13 +25,69 @@ func main() {
 	benchmark(aag, 100)
 }
 
-// Run benchmarks on the provided graphs: Compute n random routes
-func benchmark(aag *graph.AdjacencyArrayGraph, n int) {
+func readTargets(filename string) [][2]int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	runtime := 0
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	targets := make([][2]int, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) < 1 {
+			// skip empty lines
+			continue
+		} else if line[0] == '#' {
+			// skip comments
+			continue
+		}
+		var origin, destination int
+		fmt.Sscanf(line, "%d %d", &origin, &destination)
+		target := [2]int{origin, destination}
+		targets = append(targets, target)
+	}
+	return targets
+}
+
+func createTargets(n int, aag *graph.AdjacencyArrayGraph, filename string) {
+	var sb strings.Builder
 	for i := 0; i < n; i++ {
 		origin := rand.Intn(aag.NodeCount())
 		destination := rand.Intn(aag.NodeCount())
+		sb.WriteString(fmt.Sprintf("%v %v\n", origin, destination))
+	}
+	fmt.Printf("Targets:\n%s", sb.String())
+	file, cErr := os.Create(targetFile)
+
+	if cErr != nil {
+		log.Fatal(cErr)
+	}
+
+	targets := sb.String()
+	writer := bufio.NewWriter(file)
+	writer.WriteString(targets)
+	writer.Flush()
+}
+
+// Run benchmarks on the provided graphs: Compute n random routes
+func benchmark(aag *graph.AdjacencyArrayGraph, n int) {
+
+	createFile := false // set to true to recreate the target file
+	if createFile {
+		createTargets(n, aag, targetFile)
+	}
+
+	targets := readTargets(targetFile)
+
+	runtime := 0
+	for i := 0; i < n && i < len(targets); i++ {
+		origin := targets[i][0]
+		destination := targets[i][1]
 
 		navigator := path.GetNavigator(aag)
 
@@ -43,5 +104,5 @@ func benchmark(aag *graph.AdjacencyArrayGraph, n int) {
 
 		runtime += int(elapsed)
 	}
-	fmt.Printf("Average runtime: %.3fms", float64(runtime/n)/1000000)
+	fmt.Printf("Average runtime: %.3fms\n", float64(runtime/n)/1000000)
 }
