5617ea8baa7c6e26195c79e6e6ed9a5b3de64e02
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2022-08-22 20:51:48 +0200

    outsource NodeOrder

--- pkg/graph/path/contraction_hierarchies.go
+++ pkg/graph/path/contraction_hierarchies.go
@@ -34,16 +34,6 @@ type Shortcut struct {
 	via    graph.NodeId
 }
 
-type OrderItem struct {
-	nodeId             graph.NodeId
-	edgeDifference     int
-	processedNeighbors int
-	index              int
-}
-
-// NodeOrder implements the heap.Interface to hold the PriorityQueue
-type NodeOrder []*OrderItem
-
 type OrderOptions byte
 
 const (
@@ -61,19 +51,6 @@ func NewContractionHierarchiesInitialized(g graph.Graph, dijkstra *UniversalDijk
 	ch.SetShortcuts(shortcuts)
 	ch.SetNodeOrdering(nodeOrdering)
 	return ch
-}
-
-func NewOrderItem(nodeId graph.NodeId) *OrderItem {
-	return &OrderItem{nodeId: nodeId, index: -1}
-}
-
-func NewNodeOrder(initialItem *OrderItem) *NodeOrder {
-	pq := make(NodeOrder, 0)
-	heap.Init(&pq)
-	if initialItem != nil {
-		heap.Push(&pq, initialItem)
-	}
-	return &pq
 }
 
 func (ch *ContractionHierarchies) SetShortcuts(shortcuts []Shortcut) {
@@ -439,38 +416,4 @@ func (ch *ContractionHierarchies) WriteContractionResult() {
 	ch.WriteGraph()
 	ch.WriteShortcuts()
 	ch.WriteNodeOrdering()
-}
-
-func (o OrderItem) Priority() int { return o.edgeDifference + o.processedNeighbors }
-
-func (h NodeOrder) Len() int { return len(h) }
-
-func (h NodeOrder) Less(i, j int) bool { return h[i].Priority() < h[j].Priority() }
-
-func (h NodeOrder) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-	h[i].index, h[j].index = i, j
-}
-
-func (h *NodeOrder) Push(item interface{}) {
-	n := len(*h)
-	pqItem := item.(*OrderItem)
-	pqItem.index = n
-	*h = append(*h, pqItem)
-}
-
-func (h *NodeOrder) Pop() interface{} {
-	old := *h
-	n := len(old)
-	pqItem := old[n-1]
-	old[n-1] = nil
-	pqItem.index = -1 // for safety
-	*h = old[0 : n-1]
-	return pqItem
-}
-
-func (h *NodeOrder) update(pqItem *OrderItem, edgeDifference, processedNeighbors int) {
-	pqItem.edgeDifference = edgeDifference
-	pqItem.processedNeighbors = processedNeighbors
-	heap.Fix(h, pqItem.index)
 }
