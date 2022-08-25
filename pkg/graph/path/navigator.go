cd057da49bf82b1f74880eee9d3c032d5537caab
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2022-08-25 08:29:16 +0200

    update Navigator interface, GetSearchSpace

--- pkg/graph/path/navigator.go
+++ pkg/graph/path/navigator.go
@@ -1,10 +1,8 @@
 package path
 
-import "github.com/natevvv/osm-ship-routing/pkg/graph"
-
 type Navigator interface {
 	//SetGraph(g graph.Graph)
 	GetPath(origin, destination int) []int
 	ComputeShortestPath(origin, destination int) int
-	GetSearchSpace() []graph.Node
+	GetSearchSpace() []*DijkstraItem
 }
