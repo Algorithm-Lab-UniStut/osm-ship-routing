991d601e96f3be2fdbfd2a7877e03f45dba1889e
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2022-08-27 12:44:21 +0200

    old dijkstra implements Navigator interface

--- pkg/graph/path/dijkstra.go
+++ pkg/graph/path/dijkstra.go
@@ -8,21 +8,22 @@ import (
 )
 
 type Dijkstra struct {
-	g graph.Graph
+	g             graph.Graph
+	dijkstraItems []*queue.PriorityQueueItem
 }
 
-func NewDijkstra(g graph.Graph) Dijkstra {
-	return Dijkstra{g: g}
+func NewDijkstra(g graph.Graph) *Dijkstra {
+	return &Dijkstra{g: g}
 }
 
-func (d Dijkstra) GetPath(origin, destination int) ([]int, int) {
-	dijkstraItems := make([]*queue.PriorityQueueItem, d.g.NodeCount(), d.g.NodeCount())
+func (d *Dijkstra) ComputeShortestPath(origin, destination int) int {
+	d.dijkstraItems = make([]*queue.PriorityQueueItem, d.g.NodeCount(), d.g.NodeCount())
 	originItem := queue.NewPriorityQueueItem(origin, 0, -1) //{ItemId: origin, Priority: 0, Predecessor: -1, Index: -1}
-	dijkstraItems[origin] = originItem
+	d.dijkstraItems[origin] = originItem
 
 	pq := make(queue.PriorityQueue, 0)
 	heap.Init(&pq)
-	heap.Push(&pq, dijkstraItems[origin])
+	heap.Push(&pq, d.dijkstraItems[origin])
 
 	for len(pq) > 0 {
 		currentPqItem := heap.Pop(&pq).(*queue.PriorityQueueItem)
@@ -31,15 +32,15 @@ func (d Dijkstra) GetPath(origin, destination int) ([]int, int) {
 		for _, arc := range d.g.GetArcsFrom(currentNodeId) {
 			successor := arc.Destination()
 
-			if dijkstraItems[successor] == nil {
-				newPriority := dijkstraItems[currentNodeId].Priority + arc.Cost()
+			if d.dijkstraItems[successor] == nil {
+				newPriority := d.dijkstraItems[currentNodeId].Priority + arc.Cost()
 				pqItem := queue.NewPriorityQueueItem(successor, newPriority, currentNodeId) //{ItemId: successor, Priority: newPriority, Predecessor: currentNodeId, Index: -1}
-				dijkstraItems[successor] = pqItem
+				d.dijkstraItems[successor] = pqItem
 				heap.Push(&pq, pqItem)
 			} else {
-				if updatedDistance := dijkstraItems[currentNodeId].Priority + arc.Cost(); updatedDistance < dijkstraItems[successor].Priority {
-					pq.Update(dijkstraItems[successor], updatedDistance)
-					dijkstraItems[successor].Predecessor = currentNodeId
+				if updatedDistance := d.dijkstraItems[currentNodeId].Priority + arc.Cost(); updatedDistance < d.dijkstraItems[successor].Priority {
+					pq.Update(d.dijkstraItems[successor], updatedDistance)
+					d.dijkstraItems[successor].Predecessor = currentNodeId
 				}
 			}
 		}
@@ -49,17 +50,23 @@ func (d Dijkstra) GetPath(origin, destination int) ([]int, int) {
 		}
 	}
 
-	length := -1           // by default a non-existing path has length -1
-	path := make([]int, 0) // by default, a non-existing path is an empty slice
-	if dijkstraItems[destination] != nil {
-		length = dijkstraItems[destination].Priority
-		for nodeId := destination; nodeId != -1; nodeId = dijkstraItems[nodeId].Predecessor {
+	length := -1 // by default a non-existing path has length -1
+	if d.dijkstraItems[destination] != nil {
+		length = d.dijkstraItems[destination].Priority
+	}
+	return length
+}
+
+func (d *Dijkstra) GetPath(origin, destination graph.NodeId) []graph.NodeId {
+	path := make([]graph.NodeId, 0) // by default, a non-existing path is an empty slice
+	if d.dijkstraItems[destination] != nil {
+		for nodeId := destination; nodeId != -1; nodeId = d.dijkstraItems[nodeId].Predecessor {
 			path = append([]int{nodeId}, path...)
 		}
 	}
-	return path, length
+	return path
 }
 
-func (d Dijkstra) GetSearchSpace() []graph.Node {
+func (d *Dijkstra) GetSearchSpace() []*DijkstraItem {
 	panic("not implemented")
 }
