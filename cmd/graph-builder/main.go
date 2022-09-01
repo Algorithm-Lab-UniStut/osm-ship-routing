26d2178f7d96625a5d132028fa3ebe5a381e7845
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2022-09-01 21:27:48 +0200

    change debug level in graph builder

--- cmd/graph-builder/main.go
+++ cmd/graph-builder/main.go
@@ -75,10 +75,12 @@ func loadPolyJsonPolygons(file string) []geometry.Polygon {
 func createContractedGraph() {
 	fmt.Printf("Read graph file\n")
 	alg := graph.NewAdjacencyListFromFmiFile("ocean_10k.fmi")
+	//alg = graph.NewAdjacencyListFromFmiFile("ocean_equi_4.fmi")
 	dijkstra := path.NewUniversalDijkstra(alg)
 	fmt.Printf("Contract Graph\n")
 	ch := path.NewContractionHierarchies(alg, dijkstra)
-	ch.SetDebugLevel(1)
+	ch.SetDebugLevel(0)
+	ch.SetPrecomputationMilestones([]float64{0, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 99.99})
 	fmt.Printf("Initialized Contraction Hierarchies\n")
 	ch.Precompute(nil, path.MakeOrderOptions().SetDynamic(true).SetEdgeDifference(true).SetProcessedNeighbors(true).SetPeriodic(false))
 	ch.WriteContractionResult()
