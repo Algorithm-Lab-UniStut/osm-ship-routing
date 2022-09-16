f4b014694bedc8665b0a3b80b2f997056e0b357e
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2022-09-16 09:59:33 +0200

    refactor Navigator interface

--- pkg/graph/path/navigator.go
+++ pkg/graph/path/navigator.go
@@ -3,7 +3,6 @@ package path
 import "github.com/natevvv/osm-ship-routing/pkg/graph"
 
 type Navigator interface {
-	//SetGraph(g graph.Graph)
 	GetPath(origin, destination int) []int
 	ComputeShortestPath(origin, destination int) int
 	GetSearchSpace() []*DijkstraItem
